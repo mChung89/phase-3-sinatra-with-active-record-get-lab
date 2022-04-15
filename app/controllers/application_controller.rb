4deda68d8a9132f4c3df7f6f186b0531895e9508
Michael Chung <mikechung89@gmail.com>
2022-04-15 09:45:02 -0400

    commit tree 54fe399272dff1b447beaaf0f96ba7bd72f53964

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,23 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
+  get '/bakeries' do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
 
-  # add routes
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    goods = BakedGood.all.order(price: :desc)
+    goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    good = BakedGood.all.order(price: :desc).first
+    good.to_json
+  end
 
 end
